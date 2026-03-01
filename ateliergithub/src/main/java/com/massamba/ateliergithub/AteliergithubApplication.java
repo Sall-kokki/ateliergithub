8a866ad204c71979682ca440d2ebf9596be57663
Massamba Sall <massambasall866@gmail.com>
2026-03-01 23:13:16 +0100

    Initial commit

---
+++ ateliergithub/src/main/java/com/massamba/ateliergithub/AteliergithubApplication.java
@@ -0,0 +1,13 @@
+package com.massamba.ateliergithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteliergithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteliergithubApplication.class, args);
+	}
+
+}
---
+++ ateliergithub/src/test/java/com/massamba/ateliergithub/AteliergithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.massamba.ateliergithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteliergithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
